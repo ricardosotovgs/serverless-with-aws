8f65b3d721201c40a583992cd651cee8e0406ea0
Ricardo Soto Vargas <soto.v.ricardo@gmail.com>
2026-01-04 00:40:25 -0600

    feat(helloworld): add a function to test error throwing in sls

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,24 @@
 module.exports.handler = (evt, ctx, done) => {
-  
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'Hello World!',
+        input: evt
+    })
+  }
+  done(null, response)
+}
+
+// ctx: meta information about the execution environment
+module.exports.firstTask = (evt, ctx, done) => {
+    console.log('typeof evt:', typeof evt);
+    let error = null;
+    try {
+        if (evt.locations.length === 0) {
+            throw new Error("locations property is missing in the event object");
+        }
+    } catch (e) {
+        error = e;
+    }
+    done(error, evt);            
 }
